3d8c72e10483707d58545fe5cec783b254957380
EvaHaras <usenkoeva1@gmail.com>
2024-03-14 11:18:01 +0200

    refac

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-$(document).ready(function(){
+$(function() {
+    $('#nav-icon2').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menuItems = document.querySelector('.menu-items');
+  
+    menuToggle.addEventListener('click', function() {
+      menuItems.classList.toggle('active');
+    });
+  });
+
+  
+  $(document).ready(function(){
     $('.carousel').slick({
       
       infinite: true,
